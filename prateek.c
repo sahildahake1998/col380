1e433c6cd195ffbfb57dc244d0317257231e621f
sahil <sahildahakeiitdelhi@gmail.com>
2020-01-23 05:41:24 -0500

    initialise

--- prateek.c
+++ prateek.c
@@ -17,10 +17,10 @@ int main(int argc, char *argv[]){
         double **a = (double **)malloc(N * sizeof(double *));
         double **l = (double **)malloc(N * sizeof(double *));
         double **u = (double **)malloc(N * sizeof(double *));
-        printf("before pragma\n");
-        #pragma omp parallel 
-        {
-			for (i=0; i<N; i++){
+        // printf("before pragma\n");
+        // #pragma omp parallel 
+        // {
+			for (i=0; i<n; i++){
 	            a[i] = (double *)malloc(N * sizeof(double)); 
 	            l[i] = (double *)malloc(N * sizeof(double)); 
 	            u[i] = (double *)malloc(N * sizeof(double));
@@ -28,23 +28,35 @@ int main(int argc, char *argv[]){
 	        // Take a[n][n] as input
 	        for(i=0;i<n;i++){
 	            for(j=0;j<n;j++){
-		                a[i][j] = drand48()*1000;
+		                a[i][j] = drand48();
 		            }
 		        }
-		    printf("executed parallel\n");
-    	}
+		    // printf("executed parallel\n");
+    	// }
+        printf("initialisation done \n");
+        for (int i=0;i<sizeof(&a);i++) {
+            for (int j=0; j<sizeof(&a[i]);j++) {
+                printf("(%d,%d) = ",i,j);
+                printf("%f",a[i][j]);
+                printf("\n");
+            }
+        }
         
         int* pi = (int*) malloc(n * sizeof(int));;
         
+        printf("came outside pi \n");
+
         for(i=0;i<n;i++){
             for(j=0;j<n;j++){
                 u[n][n] = 0;
             }
         }
-        
+
         for(i=0;i<n;i++){
             for(j=0;j<n;j++){
+                // printf("enter for\n");
                 if(i==j){
+                    // printf("enter equal\n");
                     l[i][j] = 1;
                 }
                 else{
@@ -52,13 +64,12 @@ int main(int argc, char *argv[]){
                 }
             }
         }
-
+        
         for(i=0;i<n;i++){
             pi[i] = i;
         }
         
         printf("Entered the main forloop\n");
-
         for(k=0;k<n;k++){
             max = 0;
             for(i=k;i<n;i++){
@@ -75,33 +86,44 @@ int main(int argc, char *argv[]){
             pi[k] = pi[k_dash];
             pi[k_dash] = temp;
             //swap a(k,:) and a(k',:)
+            // #pragma omp parallel
+            // { 
             for(j=0;j<n;j++){
                 temp = a[k][j];
                 a[k][j] = a[k_dash][j];
+                // #pragma omp critical
                 a[k_dash][j] = temp;
             }
             //swap l(k,1:k-1) and l(k',1:k-1)
             for(j=0;j<k;j++){
                 temp = l[k][j];
                 l[k][j] = l[k_dash][j];
+                // #pragma omp critical
                 l[k_dash][j] = temp;
             }
             u[k][k] = a[k][k];
+            
             for(i=k+1;i<n;i++){
                 l[i][k] = a[i][k]/u[k][k];
+                // #pragma omp critical
                 u[k][i] = a[k][i];
             }
+            // }
+            // #pragma omp parallel for
             for(i=k+1;i<n;i++){
                 for(j=k+1;j<n;j++){
                     a[i][j] = a[i][j] - l[i][k]*u[k][j];
                 }
             }
+            // #pragma omp barrier
         }
         end = clock();
         printf("Time Taken: %lf\n",(double)(end - start)/CLOCKS_PER_SEC);
-        free(a);
-        free(l);
-        free(u);
+        for(int i=0; i<n;i++){
+            free(a[i]);
+            free(l[i]);
+            free(u[i]);
+        }
         free(pi);
     }
     else{
